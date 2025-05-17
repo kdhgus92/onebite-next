080092bf2ed9f9f3ef9d52f64044bc9364aa6416
ohhyeun kwon <kdhgus92@gmail.com>
2025-05-17 18:40:06 +0900

    2.14

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,26 +1,45 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import BookItem from "@/components/book-item";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import {
+  GetServerSidePropsContext,
+  GetStaticProps,
+  InferGetServerSidePropsType,
+} from "next";
 import fetchBooks from "@/lib/fetch-books";
+import { BookData } from "@/types";
+import { useRouter } from "next/router";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const q = context.query.q;
-  const books = await fetchBooks(q as string);
+// export const getStaticProps = async (
+//   context: GetStaticProps
+// ) => {
+//   const q = context.query.q; // 에러 발생
+//   const books = await fetchBooks(q as string);
 
-  return {
-    props: {
-      books,
-    },
+//   return {
+//     props: {
+//       books,
+//     },
+//   };
+// };
+
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
+
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
   };
-};
 
-export default function Page({
-  books,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  useEffect(() => {
+    if (q) {
+      fetchSearchResult();
+    }
+  }, [q]);
+
   return (
     <div>
       {books.map((book) => (
